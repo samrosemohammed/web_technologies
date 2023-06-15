0f2f10dbd6e607d687db464bd3c192f1160c9cb9
Mohammed Samrose <samrose.mohammed@gmail.com>
2023-06-15 22:38:36 +0545

    web

--- Creating_Personal_Website/script.js
+++ Creating_Personal_Website/script.js
@@ -1,42 +1,41 @@
-////////////////////////////////////////////////////////
-// Sticky navigation
-const sectionHeroEl = document.querySelector(".first-section");
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
-    if (ent.isIntersecting === false) {
-      document.body.classList.add("sticky");
-    }
-    if (ent.isIntersecting === true) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In View Port
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
+console.log("Hello world!");
 
-//////////////////////////////////////////////////////////
-// Mobile navigation works
+const myName = "Jonas Schmedtmann";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+///////////////////////////////////////////////////////////
+// Set current year
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+// yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector(".header");
+
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
-//////////////////////////////////////////////////////////
 
-// Smooth Scrolling Animation
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
 const allLinks = document.querySelectorAll("a:link");
+
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    // console.log(href);
 
     // Scroll back to top
     if (href === "#")
@@ -44,16 +43,176 @@ allLinks.forEach(function (link) {
         top: 0,
         behavior: "smooth",
       });
-    // Scroll to other link
+
+    // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
-      console.log(sectionEl);
     }
 
-    // Close mobile navigation
-    if (link.classList.contains("nav-link")) {
+    // Close mobile naviagtion
+    if (link.classList.contains("nav-link"))
       headerEl.classList.toggle("nav-open");
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".first-section");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
+
+// for button click that open my mail
+const hireBtn = document.querySelector(".btn--hire");
+const chatBtn = document.querySelector(".btn--chat");
+const hireMe = document.querySelector(".btn--hireMe");
+
+hireBtn.addEventListener("click", () => {
+  window.location.href = "mailto:samrose.mohammed@gmail.com";
+});
+
+chatBtn.addEventListener("click", () => {
+  window.open("https://www.instagram.com/bekey_9/");
+});
+
+hireMe.addEventListener("click", () => {
+  window.location.href = "mailto:samrose.mohammed@gmail.com";
+});
+
+// for social link
+const linkInsta = document.querySelector(".link-insta");
+linkInsta.addEventListener("click", function () {
+  window.open("https://www.instagram.com/bekey_9/", "_blank");
+});
+
+const linkFB = document.querySelector(".link-fb");
+linkFB.addEventListener("click", function () {
+  window.open(
+    "https://www.facebook.com/profile.php?id=100069831617699",
+    "_blank"
+  );
+});
+
+const linkLDIN = document.querySelector(".link-ln");
+linkLDIN.addEventListener("click", function () {
+  window.open(
+    "https://www.linkedin.com/in/mohammed-samrose-64b14525a/",
+    "_blank"
+  );
+});
+
+// FOOTER MAIL AND NUMBER
+const footerLinks = document.querySelectorAll(".footer-link");
+
+footerLinks.forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+    const linkHref = link.getAttribute("href");
+    if (linkHref.startsWith("tel:")) {
+      window.location.href = linkHref;
+    } else if (linkHref.startsWith("mailto:")) {
+      window.location.href = linkHref;
     }
   });
 });
+
+// when the portfolio footer is clicked
+const portfolioHeading = document.getElementById("portfolio-heading");
+
+portfolioHeading.onclick = function () {
+  location.href = "#home";
+};
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+/*
+.no-flexbox-gap .main-nav-list li:not(:last-child) {
+  margin-right: 4.8rem;
+}
+
+.no-flexbox-gap .list-item:not(:last-child) {
+  margin-bottom: 1.6rem;
+}
+
+.no-flexbox-gap .list-icon:not(:last-child) {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .delivered-faces {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .meal-attribute:not(:last-child) {
+  margin-bottom: 2rem;
+}
+
+.no-flexbox-gap .meal-icon {
+  margin-right: 1.6rem;
+}
+
+.no-flexbox-gap .footer-row div:not(:last-child) {
+  margin-right: 6.4rem;
+}
+
+.no-flexbox-gap .social-links li:not(:last-child) {
+  margin-right: 2.4rem;
+}
+
+.no-flexbox-gap .footer-nav li:not(:last-child) {
+  margin-bottom: 2.4rem;
+}
+
+@media (max-width: 75em) {
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 3.2rem;
+  }
+}
+
+@media (max-width: 59em) {
+  .no-flexbox-gap .main-nav-list li:not(:last-child) {
+    margin-right: 0;
+    margin-bottom: 4.8rem;
+  }
+}
+*/
